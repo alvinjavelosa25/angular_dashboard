325dcea1b2ab3f5bde20b8a554756d8c11f0bac6
alvinjavelosa25 <38684901+alvinjavelosa25@users.noreply.github.com>
2024-04-02 14:31:06 +0800

    Add files via upload

---
+++ src/app/layout/dashboard-layout/dashboard-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-layout',
+  templateUrl: './dashboard-layout.component.html',
+  styleUrls: ['./dashboard-layout.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class DashboardLayoutComponent {
+}
